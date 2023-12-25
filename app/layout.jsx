242dc08a3ffd0d3375a5180e57c134a1a4e48f03
AbhishekSingh <siyanisabhishek@gmail.com>
2023-12-25 11:21:13 +0530

    changed favicon icon

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,11 +5,22 @@ const metadata = {
   title: "Abhishek Singh",
   description:
     "Abhishek Singh's portfolio which contains all the projects and tech skills he have",
+  icons: {
+    icon: "/images/favicon.ico",
+  },
 };
 
 const layout = ({ children }) => {
   return (
     <html>
+      <head>
+        <link rel="icon" href="/images/favicon.ico" type="image/x-icon" />
+        <link
+          rel="shortcut icon"
+          href="/images/favicon.ico"
+          type="image/x-icon"
+        />
+      </head>
       <body>
         <Nav />
         {children}
